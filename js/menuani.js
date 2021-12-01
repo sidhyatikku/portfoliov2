c03cf160b9c87087b146ae4e692c302fd9877bfc
Sidhya Tikku <sidhyaktikku@gmail.com>
2021-11-30 19:31:44 -0500

    init

---
+++ js/menuani.js
@@ -0,0 +1,10 @@
+function menuani() {
+  var x = document.getElementById("linkh");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+
+  } else {
+    x.style.display = "block";
+  }
+
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Welcome to my personal website
+
+Protected by [CC BY-NC-ND 4.0](https://creativecommons.org/licenses/by-nc-nd/4.0/deed.en) :smile: :octocat: :rage:
